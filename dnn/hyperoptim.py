1c7e1854f45c87f3abf50e79d3876595ec4e2147
pratik.ac <pratik.ac@gmail.com>
2016-12-06 19:45:15 -0500

    hyperoptim v0.1

--- dnn/hyperoptim.py
+++ dnn/hyperoptim.py
@@ -1,4 +1,5 @@
 import os, sys, subprocess, json, argparse
+from itertools import product
 
 parser = argparse.ArgumentParser(description='Quick dirty jobber')
 parser.add_argument('-c','--command',   help='Main command', type=str, required=True)
@@ -6,9 +7,19 @@ parser.add_argument('-p','--params',    help='JSON dict of the hyper-parameters'
 parser.add_argument('-r', '--run',      help='run',  action='store_true')
 opt = vars(parser.parse_args())
 
-c = opt['command']
-p = json.loads(opt['params'])
+cmd = opt['command']
+params = json.loads(opt['params'])
 
 cmds = []
-for k in p:
-    
+gs = [1,2]
+keys,values = zip(*params.items())
+for v in product(*values):
+    p = dict(zip(keys,v))
+    s = ''
+    for k in p:
+        s += ' --'+k+' '+str(p[k])
+
+    c = cmd+s
+    c = c + (' -f -l -g %d')%(gs[len(cmds)%len(gs)])
+    cmds.append(c)
+print cmds
