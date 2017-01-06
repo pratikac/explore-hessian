a48efbb24f499506e5a39f402da972db8635ebe4
pratik.ac <pratik.ac@gmail.com>
2017-01-06 01:05:21 -0800

    added new allcnn plots

--- dnn/ploterrors.py
+++ dnn/ploterrors.py
@@ -33,97 +33,95 @@ plt.rc('legend', fontsize=fsz)
 plt.rc('figure', titlesize=fsz)
 
 smooth = lambda x,c=com: pd.ewma(x, c)
-loss = lambda df, m: df[(df['epoch'] <= m) & (df['tv'] == 1)]['loss'].iloc[::loss_subsample]
+loss = lambda df, m: df[(df['epoch'] <= m) & (df['tv'] == 1) & (df['batch'] == 2)].loss
 train = lambda df, m: df[(df['epoch'] <= m) & (df['tv'] == 1) & (df['batch'] == 0)].miss
 valid = lambda df, m: df[(df['epoch'] <= m) & (df['tv'] == 0) & (df['batch'] == 0)].miss
 
-def load(fs1, fs2, m=50):
-    dfs = []
-    r = {'loss': [], 'train': [], 'valid': []}
-    for fs in [fs1, fs2]:
-        Ds = []
-        for f in fs:
-            Ds.append(pd.read_csv(f, sep=None, engine='python'))
-        df = pd.concat(Ds, keys=[i for i in xrange(len(Ds))])
-        dfs.append(df)
+def load(fs, m=50):
+    r = {}
+    Ds = []
+    for f in fs:
+        Ds.append(pd.read_csv(f, sep=None, engine='python'))
+    df = pd.concat(Ds, keys=[i for i in xrange(len(Ds))])
 
-        r['loss'].append(loss(df, m))
-        r['train'].append(train(df, m))
-        r['valid'].append(valid(df, m))
+    r['loss'] = loss(df, m)
+    r['train'] = train(df, m)
+    r['valid'] = valid(df, m)
+
     return r
 
-def plot_lenet():
-    r = load(sorted(glob.glob('../../results/dnn/lenet/*\"langevin\":0*.log')), \
-        sorted(glob.glob('../../results/dnn/lenet/*\"langevin\":20*.log')), 100)
+# def plot_lenet():
+#     r = load(sorted(glob.glob('../results/nov_expts/dnn/lenet/*\"langevin\":0*.log')), \
+#         sorted(glob.glob('../results/dnn/nov_expts/lenet/*\"langevin\":20*.log')), 100)
 
-    fig = plt.figure(1, figsize=(8,7))
-    plt.clf()
-    ax = fig.add_subplot(111)
-    plt.title(r'LeNet: Validation error')
+#     fig = plt.figure(1, figsize=(8,7))
+#     plt.clf()
+#     ax = fig.add_subplot(111)
+#     plt.title(r'LeNet: Validation error')
 
-    sns.tsplot(np.array([smooth(r['valid'][0].ix[i]) for i in range(5)]),
-            condition=r'Adam', rasterized=True, color='k')
-    sns.tsplot(np.array([smooth(r['valid'][1].ix[i]) for i in range(4)]),
-            condition=r'entropy-SGD', rasterized=True, color='r')
+#     sns.tsplot(np.array([smooth(r['valid'][0].ix[i]) for i in range(5)]),
+#             condition=r'Adam', rasterized=True, color='k')
+#     sns.tsplot(np.array([smooth(r['valid'][1].ix[i]) for i in range(4)]),
+#             condition=r'entropy-SGD', rasterized=True, color='r')
 
-    plt.ylim([0.4, 0.8])
-    plt.xlim([0,100])
-    yticks = [0.45,.55,.65,.75]
-    plt.yticks(yticks, [str(y) for y in yticks])
-    plt.xlabel(r'Epochs')
-    plt.ylabel(r'\% Error')
+#     plt.ylim([0.4, 0.8])
+#     plt.xlim([0,100])
+#     yticks = [0.45,.55,.65,.75]
+#     plt.yticks(yticks, [str(y) for y in yticks])
+#     plt.xlabel(r'Epochs')
+#     plt.ylabel(r'\% Error')
 
-    plt.plot([98.89], [0.512], 'o', c='k', markersize=10)
-    plt.plot([81.7], [0.48], 'o', c='r', markersize=10)
+#     plt.plot([98.89], [0.512], 'o', c='k', markersize=10)
+#     plt.plot([81.7], [0.48], 'o', c='r', markersize=10)
 
-    ax.text(90, 0.54, r'$0.51\%$', fontsize=fsz,
-            verticalalignment='center', color='k')
-    ax.text(75, 0.46, r'$0.48\%$', fontsize=fsz,
-            verticalalignment='center', color='r')
+#     ax.text(90, 0.54, r'$0.51\%$', fontsize=fsz,
+#             verticalalignment='center', color='k')
+#     ax.text(75, 0.46, r'$0.48\%$', fontsize=fsz,
+#             verticalalignment='center', color='r')
 
-    plt.grid('on')
+#     plt.grid('on')
 
-    if opt['save']:
-        plt.savefig('../../doc/fig/lenet_valid.pdf', bbox_inches='tight')
+#     if opt['save']:
+#         plt.savefig('../../doc/fig/lenet_valid.pdf', bbox_inches='tight')
 
-def plot_mnistfc():
-    r = load(sorted(glob.glob('../../results/dnn/mnistfc/*\"langevin\":0*.log')), \
-        sorted(glob.glob('../../results/dnn/mnistfc/*\"langevin\":5*.log')), 100)
+# def plot_mnistfc():
+#     r = load(sorted(glob.glob('../results/nov_expts/dnn/mnistfc/*\"langevin\":0*.log')), \
+#         sorted(glob.glob('../results/dnn/nov_expts/mnistfc/*\"langevin\":5*.log')), 100)
 
-    fig = plt.figure(2, figsize=(8,7))
-    plt.clf()
-    ax = fig.add_subplot(111)
-    plt.title(r'mnistfc: Validation error')
+#     fig = plt.figure(2, figsize=(8,7))
+#     plt.clf()
+#     ax = fig.add_subplot(111)
+#     plt.title(r'mnistfc: Validation error')
 
-    v1 = np.array([smooth(r['valid'][0].ix[i]) for i in range(5)])
-    v2 = np.array([smooth(r['valid'][1].ix[i]) for i in range(5)])
-    sns.tsplot(v1,
-            condition=r'Adam', rasterized=True, color='k')
-    sns.tsplot(v2,
-            condition=r'entropy-SGD', rasterized=True, color='r')
-    plt.grid('on')
+#     v1 = np.array([smooth(r['valid'][0].ix[i]) for i in range(5)])
+#     v2 = np.array([smooth(r['valid'][1].ix[i]) for i in range(5)])
+#     sns.tsplot(v1,
+#             condition=r'Adam', rasterized=True, color='k')
+#     sns.tsplot(v2,
+#             condition=r'entropy-SGD', rasterized=True, color='r')
+#     plt.grid('on')
 
-    plt.ylim([1, 2.5])
-    plt.xlim([0,100])
-    yticks = [1, 1.5, 2, 2.5]
-    plt.yticks(yticks, [str(y) for y in yticks])
-    plt.xlabel(r'Epochs')
-    plt.ylabel(r'\% Error')
+#     plt.ylim([1, 2.5])
+#     plt.xlim([0,100])
+#     yticks = [1, 1.5, 2, 2.5]
+#     plt.yticks(yticks, [str(y) for y in yticks])
+#     plt.xlabel(r'Epochs')
+#     plt.ylabel(r'\% Error')
 
-    plt.plot([69], [1.39], 'o', c='k', markersize=10)
-    plt.plot([24], [1.39], 'o', c='r', markersize=10)
+#     plt.plot([69], [1.39], 'o', c='k', markersize=10)
+#     plt.plot([24], [1.39], 'o', c='r', markersize=10)
 
-    ax.text(66, 1.3, r'$1.39\%$', fontsize=fsz,
-            verticalalignment='center', color='k')
-    ax.text(20, 1.3, r'$1.39\%$', fontsize=fsz,
-            verticalalignment='center', color='r')
+#     ax.text(66, 1.3, r'$1.39\%$', fontsize=fsz,
+#             verticalalignment='center', color='k')
+#     ax.text(20, 1.3, r'$1.39\%$', fontsize=fsz,
+#             verticalalignment='center', color='r')
 
-    if opt['save']:
-        plt.savefig('../../doc/fig/mnistfc_valid.pdf', bbox_inches='tight')
+#     if opt['save']:
+#         plt.savefig('../../doc/fig/mnistfc_valid.pdf', bbox_inches='tight')
 
 def plot_allcnn():
-    r = load(sorted(glob.glob('../../results/dnn/allcnn/original/*.log')), \
-        sorted(glob.glob('../../results/dnn/allcnn/entropysgd/*.log')), 200)
+    r1, r2 = load(sorted(glob.glob('../results/nov_expts/dnn/allcnn/original/*.log')), 200), \
+            load(sorted(glob.glob('../results/jan_expts/allcnn10/toplot/*.log')), 10)
 
     # validation error
     fig = plt.figure(3, figsize=(8,7))
@@ -131,34 +129,32 @@ def plot_allcnn():
     ax = fig.add_subplot(111)
     plt.title(r'All-CNN-BN: Validation error')
 
-    v1 = np.array([smooth(r['valid'][0].ix[i]) for i in range(5)])
-    v2 = np.array([smooth(r['valid'][1].ix[i]) for i in range(5)])
+    v1 = np.array([smooth(r1['valid'].ix[i]) for i in range(5)])
+    v2 = np.array([smooth(r2['valid'].ix[i]) for i in range(3)])
     sns.tsplot(v1,
             condition=r'SGD', rasterized=True, color='k')
-    sns.tsplot(v2,
+    sns.tsplot(v2, time=np.arange(20,220,20),
             condition=r'entropy-SGD', rasterized=True, color='r')
     plt.grid('on')
 
-    plt.xscale('symlog',linthreshx=2)
-
-    plt.ylim([5, 45])
-    plt.xlim([0,200])
-    yticks = [5, 15, 25, 35, 45]
+    plt.ylim([5, 25])
+    plt.xlim([0, 200])
+    yticks = [5, 10, 15, 20, 25]
     plt.yticks(yticks, [str(y) for y in yticks])
-    plt.xlabel(r'Epochs')
-    plt.ylabel(r'\% Error')
+    plt.xlabel(r'\#backprops/\#batches')
+    plt.ylabel(r'\% error')
 
     plt.plot([190], [8.30], 'o', c='k', markersize=10)
-    plt.plot([13], [8.65], 'o', c='r', markersize=10)
-    plt.plot(range(200), 8.65*np.ones(200), 'r--', lw=1)
+    plt.plot([200], [8.28], 'o', c='r', markersize=10)
+    plt.plot(range(200), 8.28*np.ones(200), 'r--', lw=1)
 
-    ax.text(140, 7.0, r'$8.30\%$', fontsize=fsz,
+    ax.text(150, 10.0, r'$8.30\%$', fontsize=fsz,
             verticalalignment='center', color='k')
-    ax.text(10, 7.0, r'$8.65\%$', fontsize=fsz,
+    ax.text(150, 7.0, r'$8.28\%$', fontsize=fsz,
             verticalalignment='center', color='r')
 
     if opt['save']:
-        plt.savefig('../../doc/fig/allcnn_valid.pdf', bbox_inches='tight')
+        plt.savefig('../doc/fig/allcnn_valid.pdf', bbox_inches='tight')
 
     # training loss
     fig = plt.figure(4, figsize=(8,7))
@@ -166,38 +162,35 @@ def plot_allcnn():
     ax = fig.add_subplot(111)
     plt.title(r'All-CNN-BN: Training loss')
 
-    v1 = [smooth(r['loss'][0].ix[i][:1306], 1e-1) for i in range(5)]
-    v2 = [smooth(r['loss'][1].ix[i][:195], 1e-1) for i in range(5)]
-    xvals1 = v1[0].index.tolist()
-    xvals2 = v2[0].index.tolist()
-    sns.tsplot(v1, time = xvals1,
+    v1 = [smooth(r1['loss'].ix[i], 1e-1) for i in range(5)]
+    v2 = [smooth(r2['loss'].ix[i], 1e-1) for i in range(3)]
+    sns.tsplot(v1, time = np.arange(0,200),
             condition=r'SGD', rasterized=True, color='k')
-    sns.tsplot(v2, time = xvals2,
+    sns.tsplot(v2, time = np.arange(20,220,20),
             condition=r'entropy-SGD', rasterized=True, color='r')
     plt.grid('on')
-    plt.xscale('symlog',linthreshx=10)
 
-    plt.ylim([0, 1])
-    plt.xlim([100, xvals1[-1]])
-    xticks = [1e2, 1e3, 1e4, 5e4]
-    yticks = [0, 0.25, 0.5, 0.75, 1]
-    plt.xticks(xticks, [r'$10^2$', r'$10^3$', r'$10^4$', r'$5 \times 10^4$'])
+    plt.ylim([0,0.5])
+    plt.xlim([0, 200])
+    xticks = [0,50,100,150,200]
+    plt.xticks(xticks, [r'$0$', r'$50$', r'$100$', r'$150$', r'$200$'])
+    yticks = [0, 0.1, 0.2, 0.3, 0.4, 0.5]
     plt.yticks(yticks, [str(y) for y in yticks])
-    plt.xlabel(r'Iterations')
-    plt.ylabel(r'Cross-Entropy')
+    plt.xlabel(r'\#backprops/\#batches')
+    plt.ylabel(r'cross-entropy loss')
 
-    plt.plot([47110], [0.024377], 'o', c='k', markersize=10)
-    plt.plot([6968], [0.0161251], 'o', c='r', markersize=10)
-    plt.plot(range(xvals1[-1]), 0.0161251*np.ones(xvals1[-1]), 'r--', lw=1)
+    plt.plot([200], [0.03465], 'o', c='k', markersize=10)
+    plt.plot([200], [0.02681], 'o', c='r', markersize=10)
+    plt.plot(range(200), 0.02681*np.ones(200), 'r--', lw=1)
 
-    ax.text(35000, 0.08, r'$0.024$', fontsize=fsz,
+    ax.text(180, 0.06, r'$0.035$', fontsize=fsz,
             verticalalignment='center', color='k')
-    ax.text(5000, 0.08, r'$0.016$', fontsize=fsz,
+    ax.text(100, 0.04, r'$0.027$', fontsize=fsz,
             verticalalignment='center', color='r')
 
     if opt['save']:
-        plt.savefig('../../doc/fig/allcnn_loss.pdf', bbox_inches='tight')
+        plt.savefig('../doc/fig/allcnn_loss.pdf', bbox_inches='tight')
 
 # plot_lenet()
 # plot_mnistfc()
-plot_allcnn()
+# plot_allcnn()
